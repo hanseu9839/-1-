618a38a403aaa1213bddbd8b860589cfe3392403
hanseunggyun <hanseu9839@gmail.com>
2020-05-19 21:40:53 +0900

    TCP변수이해하기

---
+++ src/TCP/TCP02.java
@@ -0,0 +1,20 @@
+package TCP;
+
+public class TCP02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		a=1;
+		b=1;
+		c=a+b;
+		System.out.println(c);
+		
+		float f; //float, double
+		f=34.5f;
+		
+		System.out.println(f);
+	}
+
+}
