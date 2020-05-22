2ba5250bfe5b70cd17c2dd91e5f9a37e81eb78d9
hanseunggyun <hanseu9839@gmail.com>
2020-05-23 04:41:20 +0900

    TPC강의

--- src/TCP/TCP02.java
+++ src/TCP/TCP02.java
@@ -3,7 +3,7 @@ package TCP;
 public class TCP02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 프로그래밍 3대요소 : 변수 , 자료형(Data Type), 할당
 		
 		int a,b,c;
 		a=1;
@@ -15,6 +15,17 @@ public class TCP02 {
 		f=34.5f;
 		
 		System.out.println(f);
+		
+		char d;
+		d='A';
+		System.out.println(d);
+		
+		boolean g;
+		g=true; 
+		
+		System.out.println(g);
+		
+		//Book bk;//객체지향
 	}
 
 }
